e6ead6ed8066cd53585fad79408593e438daa678
ferre <ferre.w@thinkwithu.com>
2019-05-09 16:01:58 +0800

    update

--- test.go
+++ test.go
@@ -2,18 +2,9 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	// for i:=1;i<=3;i++ {
-	// 	if i>=2 {
-	// 		fmt.Print("当前的i值为：")
-	// 		fmt.Print(i)
-	// 		fmt.Print("\n")
-	// 		continue
-	// 	}
-	// 	fmt.Print("ferre\n")
-	// }
-
+	fmt.Println("satan");
+	fmt.Println("fly with me");
 }
